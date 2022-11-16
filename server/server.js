0f0432a8f564622be8a3b2241e6853d092a48ef6
Abhishek <chhadios13@gmail.com>
2022-11-16 19:05:07 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
+
+const routes = require('./routes');
+
+const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority`
+mongoose.connect(mongoUri);
+// mongoose.connect(
+//     mongoUri,
+//     { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+//     () => {
+//       console.log('Connected to MongoDB');
+//     }
+//   );
+// PARSING
+app.use(bodyParser.json())
+
+
+// SANITIZE
+app.use(xss());
+app.use(mongoSanitize());
+
+/// routes
+app.use('/api',routes)
+
+const port = process.env.PORT || 3001;
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}`)
+});
---
+++ server/services/auth.service.js
@@ -0,0 +1,49 @@
+/// MODELS
+const { User } = require('../models/user')
+// SERVICES
+const userService = require('./user.service');
+
+const createUser = async(email,password) => {
+    try{
+        if(await User.emailTaken(email)){
+            throw new Error('Sorry email taken')
+        }
+
+        const user = new User({
+            email,
+            password
+        });
+        await user.save();
+        return user;
+    } catch(error){
+        throw error
+    }
+}
+
+
+const genAuthToken = (user)=>{
+    const token = user.generateAuthToken();
+    return token;
+}
+
+const signInWithEmailAndPassword = async(email,password) =>{
+    try{
+        const user = await userService.findUserByEmail(email);
+        if(!user){
+            throw new Error('Sorry BAD email')
+        }
+        if(!(await user.comparePassword(password))){
+            throw new Error('Sorry BAD password')
+        }
+        return user;
+    } catch(error){
+        throw error
+    }
+}
+
+
+module.exports = {
+    createUser,
+    genAuthToken,
+    signInWithEmailAndPassword
+}
